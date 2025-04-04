e59661d64ddae482335cebcc9b26d4ee285c2d61
Shivam Dubey <96882359+Dubeyrock@users.noreply.github.com>
2025-04-04 17:04:28 +0530

    Update 1123. Lowest Common Ancestor of Deepest Leaves.py

--- 1123. Lowest Common Ancestor of Deepest Leaves.py
+++ 1123. Lowest Common Ancestor of Deepest Leaves.py
@@ -5,6 +5,21 @@
 #         self.left = left
 #         self.right = right
 
+
+
+
+## Approach 1: Recursion
+## Intuition
+## The problem gives a binary tree and requires returning the lowest common ancestor of its deepest leaf node. The depth of the tree's root node is 0. We note that all nodes with the maximum depth are leaf nodes. For convenience, we refer to the lowest common ancestor of the deepest leaf nodes as the lca node.
+
+## We use a recursive method to perform a depth-first search, recursively traversing each node in the tree and returning the maximum depth d of the current subtree and the lca node. If the current node is null, we return depth 0 and an null node. In each search, we recursively search the left and right subtrees, and then compare the depths of the left and right subtrees:
+
+## If the left subtree is deeper, the deepest leaf node is in the left subtree, we return {left subtree depth + 1, the lca node of the left subtree}
+## If the right subtree is deeper, the deepest leaf node is in the right subtree, we return {right subtree depth + 1, the lca node of the right subtree}
+## If both left and right subtrees have the same depth and both have the deepest leaf nodes, we return {left subtree depth + 1, current node}.
+
+
+
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def dfs(node):
